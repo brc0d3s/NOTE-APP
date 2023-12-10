1a792e37887819cdc100f4f7e864148517d9664f
brc0d3s <brc0d3s@gmail.com>
2023-12-10 09:05:42 +0300

    noteapp

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'HOST': "db.brnhihcltlhribcvqvtp.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "djangonoteapp",
+        'PORT': "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
