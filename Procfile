b1b140582712503f3753be822f74a3adb2126421
brc0d3s <brc0d3s@gmail.com>
2023-12-17 06:52:03 +0300

    noteapp

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn mynotes.wsgi --log-file -
--- mynotes/settings.py
+++ mynotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iciqweur=hp(xnyau)ev9d0@t)4orp)js8=t8qayvib05if0+3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
